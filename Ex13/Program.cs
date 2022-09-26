26473287d0768a975e976263cea2fd4c869c7ee2
anna.s <baffy.009@mail.ru>
2022-09-26 17:41:23 +0300

    Ex13

---
+++ Ex13/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Clear(); 
+ 
+Console.Write("Введите число: "); 
+ 
+int num = Convert.ToInt32(Console.ReadLine()); 
+ 
+if (num < 0) 
+{ 
+    num = -num; 
+} 
+ 
+string numstr = Convert.ToString(num); 
+ 
+if (numstr.Length > 2) 
+{ 
+  Console.WriteLine(numstr[2]); 
+} 
+else { 
+  Console.WriteLine("-> третьей цифры нет"); 
+}
