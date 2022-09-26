b5e8c7d548605fea52e0b4dab4a356f323190047
anna.s <baffy.009@mail.ru>
2022-09-26 18:04:17 +0300

    Ex15

---
+++ Ex15/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+  {
+  Console.WriteLine ("Введите день недели:");
+  int dayofweek = int.Parse (Console.ReadLine() ?? "0");
+  if (dayofweek == 6 || dayofweek == 7) Console.WriteLine ("Да");
+  else if (dayofweek <= 5) Console.WriteLine ("Нет");
+  }
